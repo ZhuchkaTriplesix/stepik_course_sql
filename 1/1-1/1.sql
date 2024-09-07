8db213763c79150c36f1947ec49f6900e99bab82
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-07 10:29:04 +0300

    ex

---
+++ 1/1-1/1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE book(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    author VARCHAR(30) NOT NULL,
+    price DECIMAL(8, 2) NOT NULL,
+    amount INT NOT NULL
+)
